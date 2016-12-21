8ed6a38a37b539155b6c3a5c66116368917b321c
Dinesh Matta <dinesh.matta@synechron.com>
2016-12-22 00:15:12 +0530

    Adding fiegro gem

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,27 @@ Bundler.require(*Rails.groups)
 module RailsDeviseRoles
   class Application < Rails::Application
 
+    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+      allow do
+        origins '*'
+
+        resource "#{ENV['tracking_url']}",
+          :headers => :any,
+          :methods => [:get, :post],
+          :credentials => true,
+          :max_age => 0
+
+        resource '*',
+          :headers => :any,
+          :methods => [:get, :post, :delete, :put, :patch, :options, :head],
+          :max_age => 0
+      end
+    end
+    
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
+
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
